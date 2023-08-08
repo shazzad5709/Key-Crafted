8bd248e207d694fb9678600580c548772d3342b8
mehzabin-haque <bsse1233@iit.du.ac.bd>
2023-08-09 01:05:08 +0600

    fixed the errors and the UI upto category

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import HeroBanner from "@/components/HeroBanner"
+import ProductCard from "@/components/ProductCard"
 import Wrapper from "@/components/Wrapper"
 
 export default function Home() {
@@ -17,7 +18,9 @@ export default function Home() {
                         during extended stretches of running.
                     </div>
                 </div>
-                <div>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 
+                gap-5 my-14 px-5 md:px-0">
+                  
                  <ProductCard />
                     <ProductCard />
                     <ProductCard />
