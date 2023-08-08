c0a8e8a861a2b36850f02a2305c416a29a3d518a
mehzabin-haque <bsse1233@iit.du.ac.bd>
2023-08-09 00:45:14 +0600

    Peoduct Card UI

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,34 @@
+import HeroBanner from "@/components/HeroBanner"
+import Wrapper from "@/components/Wrapper"
+
 export default function Home() {
   return (
     <>
       <main>
-        <h1 className="font-urbanist text-6xl p-10">Home...</h1>
+        <HeroBanner />
+        <Wrapper>
+        <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
+                    <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+                        Cushioning for Your Miles
+                    </div>
+                    <div className="text-md md:text-xl">
+                        A lightweight Nike ZoomX midsole is combined with
+                        increased stack heights to help provide cushioning
+                        during extended stretches of running.
+                    </div>
+                </div>
+                <div>
+                 <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard /> 
+                    </div>
+        </Wrapper>
       </main>
     </>
   )
